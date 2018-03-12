ae690f86c5800428b640cb39588d1041b3293a09
BJ0815 <jtt11220@gmail.com>
2018-03-12 15:36:35 +0800

    generate events_of_users model for join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312072112) do
+ActiveRecord::Schema.define(version: 20180312072353) do
 
   create_table "details", force: :cascade do |t|
     t.integer "hr"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20180312072112) do
     t.boolean "report", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events_of_users", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_events_of_users_on_event_id"
+    t.index ["user_id"], name: "index_events_of_users_on_user_id"
   end
 
   create_table "schedules", force: :cascade do |t|
