50d06e75a1152687d0a85bf54b2db54f2167e5f4
BJ0815 <jtt11220@gmail.com>
2018-03-13 20:33:53 +0800

    create search action,collection routes and page for Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,9 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+  end
+
+  def search
     @events = Event.search(params[:search])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :events
+  resources :events do
+    collection do
+      get :search
+    end
+  end
   
   root "events#index"
 end
