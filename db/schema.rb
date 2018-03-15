24034247b537123c851db644330c01ce06bda083
BJ0815 <jtt11220@gmail.com>
2018-03-15 23:18:49 +0800

    add column to users in migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312073940) do
+ActiveRecord::Schema.define(version: 20180315151552) do
 
   create_table "details", force: :cascade do |t|
     t.integer "hr"
@@ -120,6 +120,10 @@ ActiveRecord::Schema.define(version: 20180312073940) do
     t.text "introduction"
     t.integer "point", default: 0
     t.string "role", default: "normal"
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
