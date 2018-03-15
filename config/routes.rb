1df0c47086867f024d9d6e0cb63a3ba5ea6f1c0c
BJ0815 <jtt11220@gmail.com>
2018-03-15 15:20:40 +0800

    created replies controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'replies/create'
+
   devise_for :users
 
   resources :events do
+    resources :replies, only: [:create]
     collection do
       get :search
     end
