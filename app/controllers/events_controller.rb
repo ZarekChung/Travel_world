95e95b780ef89b35a5ce0b490e85d65408d8e398
Cindy Liu <cindyliu923@gmail.com>
2018-03-21 01:16:22 +0800

    change user authenticate for events/show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :search]
+  before_action :authenticate_user!, except: [:index, :search, :show]
 
   def index
   end
