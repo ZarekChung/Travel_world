78fc79142810b4e185d3e0d6e4526cd5cc28ffe0
Cindy Liu <cindyliu923@gmail.com>
2018-03-22 01:38:46 +0800

    set routes for set events step1.2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "events#index"
+
+  resources :events do 
+    member do 
+      get :schedules
+      post :schedules
+    end
+    resources :schedules
+  end 
+
 end
