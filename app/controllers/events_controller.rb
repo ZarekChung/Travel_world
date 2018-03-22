fbbc967a94a5438892f3908ebf2ea04c7ca8ebc7
Cindy Liu <cindyliu923@gmail.com>
2018-03-23 00:04:30 +0800

    setup event step2 view and controller unfinished

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,6 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-
     if @event.save
       redirect_to schedules_event_path(@event)
     else  
@@ -18,10 +17,20 @@ class EventsController < ApplicationController
     end
   end
 
+  def schedules
+    @event = Event.find(params[:id])
+    @schedule = @event.schedules.new()
+    @schedule.save
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:title, :info, :start_at, :end_at, :country, :district, :days)
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(:day, :airplane_name, :airplane_number, :airplane_terminal, :airplane_time, :stay, :check_in, :check_out, :event_id)
   end
 
 end
