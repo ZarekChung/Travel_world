c4069d504ff924d0dea4e24e5b979717d260b4d8
Cindy Liu <cindyliu923@gmail.com>
2018-03-31 06:58:24 +0800

    fix category from string to integer problem

--- db/migrate/20180312072112_create_details.rb
+++ db/migrate/20180312072112_create_details.rb
@@ -2,7 +2,7 @@ class CreateDetails < ActiveRecord::Migration[5.1]
   def change
     create_table :details do |t|
       t.integer :hr
-      t.string :category
+      t.integer :category
       t.text :content
       t.string :traffic
       t.references :schedule, foreign_key: true, index: true
