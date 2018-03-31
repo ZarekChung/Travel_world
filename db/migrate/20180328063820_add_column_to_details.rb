7376213e0ddcb8cb8240966b960578f321a60fcc
Student X <simen1127@gmail.com>
2018-03-31 13:50:59 +0800

    category type in heroku

--- db/migrate/20180328063820_add_column_to_details.rb
+++ db/migrate/20180328063820_add_column_to_details.rb
@@ -4,7 +4,7 @@ class AddColumnToDetails < ActiveRecord::Migration[5.1]
     add_column :details, :name, :string
 
     #change_column :details, :category, :integer
-    change_column :details, :category, 'integer USING CAST(column_name AS integer)'
+    change_column :details, :category, 'integer USING CAST(category AS integer)'
     rename_column :details, :category, :category_id
   end
 end
