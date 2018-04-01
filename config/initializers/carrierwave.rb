001c5d06f7333f7e6cb98b3d278fd2ffaff60db6
Zarek <simen1127@gmail.com>
2018-04-01 21:23:22 +0800

    edit setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ config.fog_credentials = {
 provider: 'AWS', # required
 aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # required
 aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-region: ['S3_BUCKET_REGION'], # optional, defaults to 'us-east-1'
+region: ENV['S3_BUCKET_REGION'] # optional, defaults to 'us-east-1'
 }
 config.fog_directory = ENV['S3_BUCKET_NAME'] # required
 config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
