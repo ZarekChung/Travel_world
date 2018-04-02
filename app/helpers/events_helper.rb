3305c669c3c16c5c7bd5c6f5db113f94e90f9ba8
BJ0815 <jtt11220@gmail.com>
2018-04-02 17:39:17 +0800

    modify privacy helper method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -24,7 +24,7 @@ module EventsHelper
   end
 
   def privacy_valid?(event, user)
-    unless @event.privacy == false
+    if @event.privacy == true
       if !user.nil?
         !event.users.find_by(id: user.id).present? 
       else
