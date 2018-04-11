0847d52060d58cf10406348b8e6168665d9ae40d
BJ0815 <jtt11220@gmail.com>
2018-04-11 13:54:22 +0800

    db bug on heroku

--- db/migrate/20180328063820_add_column_to_details.rb
+++ db/migrate/20180328063820_add_column_to_details.rb
@@ -3,8 +3,8 @@ class AddColumnToDetails < ActiveRecord::Migration[5.1]
     add_column :details, :strat_t, :datetime
     add_column :details, :name, :string
 
-    change_column :details, :category, :integer
-    #change_column :details, :category, 'integer USING CAST(category AS integer)'
+    # change_column :details, :category, :integer
+    change_column :details, :category, 'integer USING CAST(category AS integer)'
     rename_column :details, :category, :category_id
   end
 end
