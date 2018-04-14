d62337b699386b7852005601294f0ddbdf5fd166
BJ0815 <jtt11220@gmail.com>
2018-04-15 00:23:55 +0800

    change production db to pg

--- Gemfile
+++ Gemfile
@@ -88,8 +88,8 @@ gem "google_distance_matrix"
 
 
 group :production do
-  #gem 'pg', '~> 0.20'
-  gem "mysql2", '< 0.5'
+  gem 'pg', '~> 0.20'
+  #gem "mysql2", '< 0.5'
 end
 
 group :development, :test do
--- db/migrate/20180328063820_add_column_to_details.rb
+++ db/migrate/20180328063820_add_column_to_details.rb
@@ -3,8 +3,7 @@ class AddColumnToDetails < ActiveRecord::Migration[5.1]
     add_column :details, :strat_t, :datetime
     add_column :details, :name, :string
 
-    change_column :details, :category, :integer
-    #change_column :details, :category, 'integer USING CAST(category AS integer)'
-    rename_column :details, :category, :category_id
+    remove_column :details, :category
+    add_column :details, :category_id, :integer
   end
 end
